3fe57c4530549b9c18414d4918ec3c09c479dd8f
Rawle Juglal <rawledjuglal@gmail.com>
2017-02-25 21:52:58 +0000

    added(programs pages):added html and css for programs views

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,25 @@ router.get('/main', function(req, res, next){
 router.get('/instructors', function(req, res, next ){
   res.render('instructors', { title: 'ATTOKC - Instructors'});
 });
+
+router.get('/BJJ', function(req, res, next){
+  res.render('bjj', { title: 'ATTOKC - Brazilian JiuJitsu'});
+});
+
+router.get('/Wrestling', function(req, res, next){
+  res.render('wrestling', { title: 'ATTOKC - Wrestling'});
+});
+
+router.get('/Kids', function(req, res, next){
+  res.render('kids', { title: 'ATTOKC - Kids BJJ'});
+});
+
+router.get('/Kickboxing', function(req, res, next){
+  res.render('kickboxing', { title: 'ATTOKC - Kick Boxing'});
+});
+
+router.get('/NoGi', function(req, res, next){
+  res.render('noGi', { title: 'ATTOKC - No Gi'});
+});
+
 module.exports = router;
